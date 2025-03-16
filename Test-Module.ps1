a6c621f23a089f9af798712ef615d76d8c74694b
alain <79479952+chadnpc@users.noreply.github.com>
2025-03-16 20:38:57 +0200

    Update Test-Module.ps1

--- Test-Module.ps1
+++ Test-Module.ps1
@@ -1,10 +1,10 @@
 ﻿
 #!/usr/bin/env pwsh
 # .SYNOPSIS
-#   PoshRest testScript v0.1.0
+#   PoshRest testScript v0.1.1
 # .EXAMPLE
-#   ./Test-Module.ps1 -version 0.1.0
-#   Will test the module in ./BuildOutput/PoshRest/0.1.0/
+#   ./Test-Module.ps1 -version 0.1.1
+#   Will test the module in ./BuildOutput/PoshRest/0.1.1/
 # .EXAMPLE
 #   ./Test-Module.ps1
 #   Will test the latest  module version in ./BuildOutput/PoshRest/
