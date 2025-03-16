f21a73c0c6b0c85feb14dba7dc681018fac7089e
alain <79479952+chadnpc@users.noreply.github.com>
2025-03-16 20:39:17 +0200

    Delete Handle-PoshRestError.ps1

--- Public/Handle-PoshRestError.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Handle-PoshRestError {
-  param(
-    [Parameter(Mandatory)]
-    [PoshRestResponse]$Response
-  )
-  if (-not $Response.IsSuccessful) {
-    throw "HTTP Error: $($Response.StatusCode) - $($Response.Content)"
-  }
-}
