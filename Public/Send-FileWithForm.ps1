ef3b524f743f2ff53699700e648536f4a8680335
alain <79479952+chadnpc@users.noreply.github.com>
2025-03-16 20:39:45 +0200

    Create Send-FileWithForm.ps1

---
+++ Public/Send-FileWithForm.ps1
@@ -0,0 +1,36 @@
+﻿function Send-FileWithForm {
+  [CmdletBinding()][Alias('Upload-FileWithForm')]
+  param(
+    [Parameter(Mandatory)]
+    [string]$Uri,
+
+    [Parameter(Mandatory)]
+    [string]$FilePath,
+
+    [hashtable]$FormFields = @{},
+    [string]$FileParameterName = "file"
+  )
+
+  process {
+    # Create client
+    $client = [PoshRest]::new($Uri.Split('/')[2..3] -join '/')
+
+    # Build request
+    $resource = $Uri.Split('/')[-1]
+    $request = [PoshRestRequest]::new($resource, [System.Net.Http.HttpMethod]::Post)
+    $request.AddFile($FileParameterName, $FilePath)
+
+    # Add form fields
+    foreach ($field in $FormFields.GetEnumerator()) {
+      $request.AddParameter($field.Name, $field.Value, [ParameterType]::Body)
+    }
+
+    # Execute upload
+    $response = $client.Execute($request)
+    if ($response.IsSuccessful) {
+      Write-Host "Upload successful: $($response.StatusCode)"
+    } else {
+      Write-Error "Upload failed: $($response.Content)"
+    }
+  }
+}
